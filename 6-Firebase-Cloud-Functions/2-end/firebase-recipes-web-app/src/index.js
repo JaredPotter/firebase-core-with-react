2a9d1d0647e2881670e00a7f156a4c7a257c9c04
Jared Potter <jaredpotter1@gmail.com>
2020-11-15 04:27:23 -0700

    Fix correct App.js

--- 6-Firebase-Cloud-Functions/2-end/firebase-recipes-web-app/src/index.js
+++ 6-Firebase-Cloud-Functions/2-end/firebase-recipes-web-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import App from './App_client_firestore';
+import App from './App';
+// import App from './App_client_firestore';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
