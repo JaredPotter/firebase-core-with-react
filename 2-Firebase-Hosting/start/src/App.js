813e32c265c8d4f4acd83b62e4857738c9456b8d
Jared Potter <jaredpotter1@gmail.com>
2020-11-16 20:18:26 -0700

    Section 2 - start

---
+++ 2-Firebase-Hosting/start/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
